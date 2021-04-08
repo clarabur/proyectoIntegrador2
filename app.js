410720d8dddf0f62094c2461089b36961bcadc7f
Daira Sapir <dairasapir@MacBook-Air-de-Daira.local>
2021-04-07 21:06:23 -0300

    ya hice las rutas

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var productRouter = require('./routes/product');
+var profileRouter = require('./routes/profile');
+var registerRouter = require('./routes/register');
+var searchResultsRouter = require('./routes/search-results');
 
 var app = express();
 
@@ -21,6 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/product', productRouter);
+app.use('/profile', profileRouter);
+app.use('/register', registerRouter);
+app.use('/search-results', searchResultsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
