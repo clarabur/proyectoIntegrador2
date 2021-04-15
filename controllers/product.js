81bcccbde5a94ce5262eb176798380d690017bc6
Your Name <you@example.com>
2021-04-15 12:27:10 -0300

    foton de registro fuciona

--- controllers/product.js
+++ controllers/product.js
@@ -3,10 +3,8 @@ let productController = {
 
     product: (req, res)=>{
         res.render ("product")
-    },
-    productadd: (req, res)=>{
-        res.render ("product-add")
     }
+
 }
 
 module.exports = productController
