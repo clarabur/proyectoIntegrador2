92382807021e80d2dcabcf8666c64c1e82533b97
Your Name <you@example.com>
2021-04-19 11:27:36 -0300

    cambios

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use('/register', registerRouter);
 app.use('/search-results', searchResultsRouter);
 app.use('/product-add', productaddRouter);
 app.use('/profile-edit', profileeditRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ data/products.js
@@ -0,0 +1,22 @@
+
+let products =[
+    {
+        nombre: "blazer negro",
+        id: 1,
+        imagen:"/images/products/bazer.jpeg",
+
+    },
+    {
+        nombre:"saco largo",
+        id:2,
+        imagen:"/images/products/nueva.webp",
+        
+    },
+    {
+        nombre:"jean oversize",
+        id:3,
+        imagen:"/images/products/jeanblanco.jpeg",
+        
+    }
+]
+module.exports=products
