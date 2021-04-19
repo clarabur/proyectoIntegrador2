6bdf576e68eac163923d014ef73275b9bd63b859
Daira Sapir <dairasapir@MacBook-Air-de-Daira.local>
2021-04-19 14:50:17 -0300

    data products ok

--- data/products.js
+++ data/products.js
@@ -12,13 +12,51 @@ let products =[
         nombre:"saco largo",
         id:2,
         imagen:"/images/products/nueva.webp",
+        descripcion: "Saco de invierno cuadrille con botones",
+        comentarios:"Comentarios: 4",
         
     },
     {
-        nombre:"jean oversize",
-        id:3,
+        nombre: "jean oversize",
+        id: 3,
         imagen:"/images/products/jeanblanco.jpeg",
-        
-    }
+        descripcion:"Jean oversize blanco tiro alto",
+        comentarios:"Comentarios: 4",
+    },
+    {
+        nombre: "remera clasica",
+        id: 4,
+        imagen:"/images/products/remera3.jpeg",
+        descripcion:"remera de algodon blanca clasica",
+        comentarios:"Comentarios: 4",
+    },
+    {
+        nombre: "chaleco",
+        id: 5,
+        imagen:"/images/products/saco.jpeg",
+        descripcion:"chaleco de ultima moda con botones",
+        comentarios:"Comentarios: 4",
+    },
+    {
+        nombre: "jean negro",
+        id: 6,
+        imagen:"/images/products/jean.jpeg",
+        descripcion:"jean de todos los tiempos, tiro alto, modelo fit",
+        comentarios:"Comentarios: 4",
+    },
+    {
+        nombre: "vestido negro",
+        id: 7,
+        imagen:"/images/products/vestido.jpeg",
+        descripcion:"vestido largo negro con encaje al cuerpo",
+        comentarios:"Comentarios: 4",
+    },
+    {
+        nombre: "vestido rosado",
+        id: 8,
+        imagen:"/images/products/vestido2.jpeg",
+        descripcion:"vestido de ceda, largo, suelto",
+        comentarios:"Comentarios: 4",
+    },
 ]
 module.exports=products
