a95e4ac9a23aa143416474cefe6ab0ce3abdbcb5
Your Name <you@example.com>
2021-06-05 18:23:31 -0300

    cambios

---
+++ data/profile.js
@@ -0,0 +1,26 @@
+
+let profiles ={
+lista: [
+    {
+id: 1,
+nombre:"Daira Sapir " ,
+imagen: "/images/foto 1.jpeg",
+mensaje:" Excenlente calidad!" ,
+fecha: "2020-10-4"							
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+}
+
+
+
+module.exports=profiles
