9b36cf56c17a61ebadd9410e2852d87c6c176478
Your Name <you@example.com>
2021-06-13 19:27:22 -0300

    cambios en el perfil

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,10 +1,25 @@
 
 var detalleProducto  = require ("../data/products")
+const db = require ('../database/models')
+
+const usuarios = db.Usuario
+
+const op = db.sequelize.Op
 let profileController = {
 
     profile: (req, res)=>{
+        
+          /*   Usuario.findAll()
+          
+         .then((resultados)=> res.render('profile', {resultados}))
+              .catch((err)=> `Error: ${err}`) 
+ 
+         */
         res.render ("profile", {productos: detalleProducto.lista})
     },
+    profileedit: (req, res)=>{
+        res.render ("profile-edit")
+    }
 
 }
 
