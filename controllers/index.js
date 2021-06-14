0ac56ebe9a72c956f453d1c3eaab7bf21513e83b
Cala Ruggeri <calaruggeri@MacBook-Pro-de-Cala.local>
2021-06-14 18:54:35 -0300

    cambios en el index

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const db = require ('../database/models')
+const db = require('../database/models')
 
 const productos = db.Producto
 
@@ -8,51 +8,63 @@ const op = db.sequelize.Op;
 
 let indexController = {
 
-    index: (req, res)=>{  
-      
-      productos.findAll()
-          
-         .then((resultados)=> 
-        
-        res.render('index', {resultados})
-         )
-         .catch((err)=> `Error: ${err}`) 
-        },
-        show: (req, res)=>{
-              let primaryKey = req.params.id;
-            productos.findByPk(primaryKey, {
-              include: [{association: 'comentario'},{association: 'usuario'} ]
-            })
-         
-              .then(resultados =>   
-                //console.log (resultados)
-                res.render('product', {resultados}) )                
-                .catch((err)=> `Error: ${err}`) 
-                 
-             
-        },
-         search: (req,res)=> {
-          let buscadorProductos = req.query.search
-          productos.findAll({
-            where: [
-              { nombre: { [op.like]: `%${buscadorProductos}%` } }
-            ]
-          })
-          .then(resultados => res.render('search-results', {resultados:resultados}) )
-            .catch((err)=>console.log (err) ) 
-        },
-       
-    // ordenar productos de manera descendentes
+  index: (req, res) => {
 
-  store: (req, res)=>{
+    db.Producto.findAll()
+
+      .then(resultados => {
+
+        res.render('index', {
+          resultados: resultados
+        })
+      })
+      .catch((err) => `Error: ${err}`)
+  },
+  show: (req, res) => {
+    let primaryKey = req.params.id;
+    productos.findByPk(primaryKey, {
+        include: [{
+          association: 'comentario'
+        }, {
+          association: 'usuario'
+        }]
+      })
+
+      .then(resultados =>
+        //console.log (resultados)
+        res.render('product', {
+          resultados
+        }))
+      .catch((err) => `Error: ${err}`)
+
+
+  },
+  search: (req, res) => {
+    let buscadorProductos = req.query.search
     db.Producto.findAll({
-        order: [
-            ['lanzamiento', 'DESC']
-        ],
+        where: [{
+          nombre: {
+            [op.like]: `%${buscadorProductos}%`
+          }
+        }]
+      })
+      .then(resultados => res.render('search-results', {
+        resultados: resultados
+      }))
+      .catch((err) => console.log(err))
+  },
 
-       
-  })
- },
+  // ordenar productos de manera descendentes
+
+  store: (req, res) => {
+    db.Producto.findAll({
+      order: [
+        ['lanzamiento', 'DESC']
+      ],
+
+
+    })
+  },
 
 }
 
