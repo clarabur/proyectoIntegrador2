d1c86a5aa431c78e994b5ba7655a938b9835efd1
Your Name <you@example.com>
2021-06-14 11:31:14 -0300

    cambios

--- controllers/search-results.js
+++
@@ -1,9 +0,0 @@
-var detalleProducto  = require ("../data/products")
-let searchController = {
-
-    search: (req, res)=>{
-        res.render ("search-results", {productos: detalleProducto.lista})
-    }
-}
-
-module.exports = searchController
--- routes/search-results.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const searchController = require('../controllers/search-results');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/',searchController.search )
-
-module.exports = router;
