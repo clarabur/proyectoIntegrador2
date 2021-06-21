8276254d29983414183dc271047fbc71fd0c6c18
Your Name <you@example.com>
2021-06-21 16:40:45 -0300

    punto 8 casi completo

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', indexController.index) 
+
 router.get('/detalle/:id', indexController.show);
 router.get('/search', indexController.search)
 router.post('/detalle/:id', indexController.destroy);
 router.get('/borrar/:id', indexController.borrar)
-
+router.get('/edit/:id', indexController.edit)
+router.post('/edit/:id', indexController.update)
 module.exports = router;
